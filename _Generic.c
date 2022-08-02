b01be7140607dc279f24ab19abe584229aada3a1
PrairieFire2b <110272607+PrairieFire2b@users.noreply.github.com>
2022-08-02 09:44:48 +0800

    Update _Generic.c

--- _Generic.c
+++ _Generic.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+#define learn_print_function(x) _Generic(         \
+    (x),                                          \
+    char: char_print,                             \
+    char*: char_pointer_print,                    \
+    unsigned char: unsigned_char_print,           \
+    short: short_print,                           \
+    unsigned short: unsigned_short_print,         \
+    int: int_print,                               \
+    unsigned int: unsigned_int_print,             \
+    long long: long_long_print,                   \
+    unsigned long long: unsigned_long_long_print, \
+    float: float_print,                           \
+    double: double_print,                         \
+    long double: long_double_print,               \
+    void*: pointer_print                          \
+)(x)
+
 #define learn_typeof(x) _Generic(           \
     (x),                                    \
     char: char,                             \
@@ -13,8 +30,8 @@
     float: float,                           \
     double: double,                         \
     long double: long double                \
-)                                           \
- 
+)
+
 #define learn_typenameof(x) _Generic(         \
     (x),                                      \
     char: "char",                             \
@@ -28,7 +45,7 @@
     float: "float",                           \
     double: "double",                         \
     long double: "long_double"                \
-)                                             \
+)
 
 #define learn_typenameof_function(x) _Generic(   \
     (x),                                         \
@@ -43,7 +60,7 @@
     float: float_name,                           \
     double: double_name,                         \
     long double: long_double_name                \
-)(x)                                             \
+)(x)
 
 
 char* char_name() { return "char"; }
@@ -67,3 +84,29 @@ char* float_name() { return "float"; }
 char* double_name() { return "double"; }
 
 char* long_double_name() { return "long double"; }
+
+void char_print(char x) { printf("%c", x); }
+
+void char_pointer_print(char* x) { printf("%s", x); }
+
+void unsigned_char_print(unsigned char x) { printf("%c", x); }
+
+void short_print(short x) { printf("%d", x); }
+
+void unsigned_short_print(unsigned short x) { printf("%u", x); }
+
+void int_print(int x) { printf("%ld", x); }
+
+void unsigned_int_print(unsigned int x) { printf("%lu", x); }
+
+void long_long_print(long long x) { printf("%lld", x); }
+
+void unsigned_long_long_print(unsigned long long x) { printf("%llu", x); }
+
+void float_print(float x) { printf("%f", x); }
+
+void double_print(double x) { printf("%f", x); }
+
+void long_double_print(long double x) { printf("%Lf", x); }
+
+void pointer_print(void* x) { printf("%p", x); }
